f44f0c4ddd9d76754a250428da08fcc3b4ae9a35
Midhet Sulemani <midhetfatema94@gmail.com>
2020-12-08 19:09:29 +0300

    initial commit and even odd fills

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Waveline Media on 12/8/20.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# DrawingSwiftUI
+
+A repository with the learnings of drawing custom and built in shapes in SwiftUI with Core Graphics.
+
+Features are:
+* Drawing paths and shapes in SwiftUI
+* Making custom shapes
+* Even Odd fill concepts
